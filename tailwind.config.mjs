0b11c147ceeff1b453a98722799c38f22a2cc9f6
fluqont <fluqont@gmail.com>
2024-01-08 20:30:17 +0300

    feat: self-host fonts and dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,mdx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "red-rose": ["Red Rose", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

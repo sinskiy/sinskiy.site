499d0dc0fd305a89bc5aa2e9a90fb24946a084a0
sinskiy <sinskiy@proton.me>
2024-09-03 19:51:41 +0300

    init

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-let orderClick = function (underline) {
-  const link = document.querySelectorAll(".order__info__link");
-  link.forEach((element) => {
-    if (element.hasAttribute("data-active", "yes")) {
-      element.setAttribute("data-active", "");
-    }
-  });
-  underline.setAttribute("data-active", "yes");
-};

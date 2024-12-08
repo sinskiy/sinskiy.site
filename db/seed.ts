7b1a909b8a60a3ef209d6318eaec929ff03d75bd
fluqont <fluqont@gmail.com>
2024-12-08 15:11:19 +0300

    feat: thoughts (1)

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,9 @@
-import { db } from 'astro:db';
+import { db, Thought } from "astro:db";
 
-// https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Thought).values([
+    { username: "sinskiy", message: "I have no thoughts" },
+    { username: "Сева", message: "Ты дурак" },
+    { username: "kilwinta", message: "Давай в Roblox" },
+  ]);
 }

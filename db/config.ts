8eb8a5f504b24c08d7448a2cd73a60868169c5c9
sinskiy <dimasinskiy@gmail.com>
2025-06-04 00:28:48 +0300

    fix: default date

--- db/config.ts
+++ db/config.ts
@@ -1,11 +1,11 @@
-import { column, defineDb, defineTable } from "astro:db";
+import { column, defineDb, defineTable, sql } from "astro:db";
 
 const Thought = defineTable({
   columns: {
     id: column.text({ primaryKey: true }),
     username: column.text(),
     message: column.text(),
-    date: column.date({ default: new Date() }),
+    date: column.date({ default: sql`(CURRENT_TIMESTAMP)` }),
     post: column.text({ optional: true }),
   },
 });
